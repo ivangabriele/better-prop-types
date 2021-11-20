b8fab04b32921d1826c4d486676a29c9ea1f0a89
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-11-20 15:24:17 +0100

    test: collect coverage from all source files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
+  collectCoverageFrom: ['src/**/*.ts'],
+  maxWorkers: '50%',
   preset: 'ts-jest',
   testEnvironment: 'node',
 }
