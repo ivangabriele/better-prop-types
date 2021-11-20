d783376562926cbc51e2de505c68f40854289298
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-11-20 20:20:06 +0100

    fix: update isNotNull & isNullable types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Requireable, Validator } from 'prop-types'
 
-export interface Isable<T> extends Requireable<T> {
+export interface Isable<T> extends Requireable<T | undefined | null> {
   /** Can be undefined but NOT null. */
-  isNotNull: Validator<NonNullable<T>>
+  isNotNull: Validator<T | undefined>
   /** Is required but can be null. */
-  isNullable: Validator<NonNullable<T>>
+  isNullable: Validator<T | null>
 }
